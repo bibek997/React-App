43288c329049bd66195be61046dc8070d58932d3
bibek997 <bibekmagar997@gmail.com>
2024-07-03 23:59:57 +0545

    updated pcps

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <section
-    class="flex flex-col md:flex-row items-center justify-between px-6 md:px-20 bg-grey-200"
+    class="flex flex-col md:flex-row items-center justify-between px-6 md:px-20 bg-grey-200 rounded-none"
     id="home"
   >
     <div class="md:w-1/2">
       <h1 class="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
       PCPS College
       </h1>
-      <p class="text-gray-700 mb-6">
+      <p class="text-gray-700 mb-1 text-justify mr-2">
       PCPS College is a multilingual, diverse, multicultural institution of higher
       education that ensures intellectual research, and informed teaching. PCPS College
       offers international(UK) undergraduate and graduate programs in various fields of study.
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Home from '../components/Home'
+import Home from '../components/HeroSection'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Contact from '../components/Contact'
 
 const Homepage = () => {
   return (
     <div>
         <Navbar/>
         <Home/>
+        <Contact/>
+        <Footer/>
+        
     </div>
   )
 }
